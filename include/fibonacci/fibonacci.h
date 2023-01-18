38e96248c8151b11589ac6c8af6e960b18a14612
Ian Dinwoodie <dinwoodieian@gmail.com>
2023-01-17 20:59:01 -0500

    Added space complexity notes.

--- include/fibonacci/fibonacci.h
+++ include/fibonacci/fibonacci.h
@@ -4,14 +4,17 @@
 namespace fibonacci {
 // Returns the Nth Fibonacci number using a naive recursive implementation.
 // Time: O(n^2)
+// Space: O(n)
 unsigned int recursive_naive(unsigned int n);
 
 // Returns the Nth Fibonacci number using a memoized recursive implementation.
 // Time: O(n)
+// Space: O(n)
 unsigned int recursive_memo(unsigned int n);
 
 // Returns the Nth Fibonacci number using an iterative implementation.
 // Time: O(n)
+// Space: O(1)
 unsigned int iterative(unsigned int n);
 } // namespace fibonacci
 
